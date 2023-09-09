a2d2d65c57c7bedd40c28f3b572d7b079d014531
kongtaewii <144503084+kongtaewii@users.noreply.github.com>
2023-09-09 12:05:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
 import streamlit as st
 
 st.title('📝Some problems for League Of Legends players📝')
-
-font_dirs = [os.getcwd() + '/customFonts']
-font_files = fm.findSystemFonts(fontpaths=font_dirs)
-
-for font_file in font_files:
-    fm.fontManager.addfont(font_file)
-fm._load_fontmanager(try_read_cache=False)
