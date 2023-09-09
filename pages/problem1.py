e1ecc230a6f0bcefd221c8cfecbbeb14fea5b23a
kongtaewii <144503084+kongtaewii@users.noreply.github.com>
2023-09-09 12:19:35 +0900

    Update problem1.py

--- pages/problem1.py
+++ pages/problem1.py
@@ -12,7 +12,7 @@ st.image(potion)
 st.markdown('체력 물약은 사용시 15초에 걸쳐 120의 체력을 회복합니다.'
             ' 개당 가격은 50골드이며, 상황은 다음과 같습니다.')
 
-st.markdown('**아 우리 서폿 왜 5포션 시작이냐? :blue[x]초 동안 :blue[y]의 체력을 회복하는건 너무 비효율적인거 아님?**')
+st.markdown('**아 우리 서폿 왜 갑자기 5포션 다마심? :blue[x]초 동안 :blue[y]의 체력을 회복하는건 너무 비효율적인거 아님?**')
 
 problem = st.number_input(
     '이 때의 :blue[x+y]값을 구하시오.', min_value=0,
